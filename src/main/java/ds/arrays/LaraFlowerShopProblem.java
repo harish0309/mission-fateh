7ae2bbb8987f4c8d00419ff7834463a5a5f4da59
harish0309 <harish0309@gmail.com>
2019-09-25 22:27:57 +0530

    Lara Flowers Shop problem

--- src/main/java/ds/arrays/LaraFlowerShopProblem.java
+++ src/main/java/ds/arrays/LaraFlowerShopProblem.java
@@ -1,81 +1,36 @@
 package ds.arrays;
 
-import static java.lang.Math.max;
-
 /**
  * Created by HARISH on 24- 09- 2019
  */
 public class LaraFlowerShopProblem {
 
     public static void main(String[] args) {
-        System.out.println(maxProfit(5, 3, new int[]{0, 0, 0, 1}));
+        System.out.println(maxProfit(2, 3, new int[]{0, 0, 0, 1/*, 1, 0, 1, 0, 1, 1*/}));
 
     }
 
     static int maxProfit(int pT1, int pT2, int[] a) {
-        int profitType1 = 0;
-        int profitType2 = 0;
-        int profitType1n2 = 0;
-        int n = a.length;
-
-        int countType1 = 0;
-        int countType2 = 0;
-        int i = 0;
-
-        // only for type 1
-        int count1 = 0;
-
-        while (i < n - 2) {
+        int[] sol = new int[a.length];
+        for (int i = 1; i < a.length; i++) {
             if (isType1(i, a)) {
-                count1++;
-                i = i + 3;
-            } else {
-                i++;
-            }
-        }
-
-        profitType1 = count1 * pT1;
-
-        // only for type 2
-        int count2 = 0;
-        i = 0;
-
-        while (i < n - 1) {
-            if (isType2(i, a)) {
-                count2++;
-                i = i + 2;
-            } else {
-                i++;
-            }
-        }
-
-        profitType2 = count2 * pT2;
-
-        i = 0;
-        while (i < n - 2) {
-            if (isType1(i, a)) {
-                countType1++;
-                i = i + 3;
+                sol[i] = Math.max(sol[i - 1], pT1 + sol[i - 3]);
             } else if (isType2(i, a)) {
-                countType2++;
-                i = i + 2;
+                sol[i] = Math.max(sol[i - 1], pT2 + sol[i - 2]);
             } else {
-                i++;
+                sol[i] = sol[i - 1];
             }
         }
 
-        profitType1n2 = (countType1 * pT1) + (countType2 * pT2);
-
-
-        return max(max(profitType1, profitType2), profitType1n2);
+        return sol[a.length - 1];
     }
 
     private static boolean isType2(int i, int[] a) {
-        return (a[i] == 0 && a[i + 1] == 1) || (a[i] == 1 && a[i + 1] == 0);
+        return i - 2 >= 0 && ((a[i] == 0 && a[i - 1] == 1) || (a[i] == 1 && a[i - 1] == 0));
     }
 
     private static boolean isType1(int i, int[] a) {
-        return a[i] == 0 && a[i + 1] == 0 && a[i + 2] == 0;
+        return i - 3 >= 0 && (a[i] == 0 && a[i - 1] == 0 && a[i - 2] == 0);
     }
 
 }
