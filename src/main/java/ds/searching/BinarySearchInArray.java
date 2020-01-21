ff1e33b9ced5f1b3e8c0a71eb93db9127eed010c
harish0309 <harish0309@gmail.com>
2020-01-22 00:05:02 +0530

    Binary Search Final

--- src/main/java/ds/searching/BinarySearchInArray.java
+++ src/main/java/ds/searching/BinarySearchInArray.java
@@ -1,24 +1,31 @@
 package ds.searching;
 
-/** Created by HARISH on 18- 03- 2019 */
+/**
+ * Created by HARISH on 18- 03- 2019
+ */
 public class BinarySearchInArray {
-  public static void main(String[] args) {
-    int[] A = {2, 4, 6, 8, 10, 12, 14};
-    int size = A.length;
-    int find = 11;
-    int start = 0;
-    int end = size - 1;
-    int mid = 0;
-    while (start <= end) {
-      mid = start + (end - start) / 2;
-      if (find < A[mid]) {
-        end = mid - 1;
-      } else if (find > A[mid]) {
-        start = mid + 1;
-      } else if (find == A[mid]) {
-        System.out.println("Element found at index {" + mid + "}");
-        break;
-      }
+    public static void main(String[] args) {
+        int[] A = new int[]{2, 4, 8, 10, 15, 17, 19};
+        int s = 0;
+        int e = A.length;
+        int idx = -1;
+        int num = 18;
+
+        while (s < e) {
+            int mid = (s + e) / 2;
+            if (num == A[mid]) {
+                idx = mid;
+                break;
+            }
+            if (num < A[mid]) {
+                e = mid;
+            } else {
+                s = mid + 1;
+            }
+        }
+        idx = idx != -1 ? idx : s;
+
+        System.out.println("Given number " + num + " should be at index {" + idx + "}");
+
     }
-  }
 }
