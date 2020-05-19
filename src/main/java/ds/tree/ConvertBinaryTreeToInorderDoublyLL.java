f52f9a0a715f98dd77f93ba91cff3b4258b79eda
harish0309 <harish0309@gmail.com>
2020-05-19 20:50:31 +0530

    Convert Binary Tree to Inorder Doubly Linked List, it should be circular

---
+++ src/main/java/ds/tree/ConvertBinaryTreeToInorderDoublyLL.java
@@ -0,0 +1,49 @@
+package ds.tree;
+
+import ds.tree.BST.Node;
+
+import java.util.Stack;
+
+/** Created by HARISH on 19- 05- 2020 */
+public class ConvertBinaryTreeToInorderDoublyLL {
+
+  public static void main(String[] args) {
+
+    BST bst = new BST();
+    bst.add(10);
+    bst.add(5);
+    bst.add(20);
+    bst.add(3);
+    bst.add(8);
+    bst.add(9);
+    bst.add(15);
+    bst.add(30);
+
+    Node root = bst.getRoot();
+    Node head = null;
+    Node prev = null;
+    Stack<Node> s = new Stack<>();
+
+    while (true) {
+      if (root != null) {
+        s.push(root);
+        root = root.left;
+      } else {
+        if (s.isEmpty()) break;
+        Node n = s.pop();
+        if (head == null) {
+          head = n;
+        }
+        if (prev != null) {
+          prev.right = n;
+        }
+        n.left = prev;
+        prev = n;
+        root = n.right;
+      }
+    }
+
+    head.left = prev;
+    prev.right = head;
+  }
+}
